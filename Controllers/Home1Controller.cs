7a44a56389f1d5138f6f5574afecea4fd9fe21f9
nemanja-mihailov <nemanjamihailov0@gmail.com>
2022-11-11 14:20:51 +0100

    first commit

---
+++ Controllers/Home1Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        // GET: Home1
+        public ActionResult Index()
+        {
+            return View("~/Views/Index/Index.cshtml", new CalcModel());
+        }
+        [HttpPost]
+        public ActionResult Index(CalcModel calcModel)
+        {
+            double dinar1 = calcModel.euro1 * 120;
+            calcModel.dinar1 = dinar1;
+            ModelState.Clear();
+
+            return View("~/Views/Index/Index.cshtml", calcModel);
+
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("~/ Views / Index / Index.cshtml", new CalcModel());
+        }
+        [HttpPost]
+       public ActionResult Index(CalcModel calcModel)
+        {
+            double euro = calcModel.dinar / 118;
+            calcModel.euro = euro;
+            ModelState.Clear();
+
+            return View("~/Views/Index/Index.cshtml", calcModel);
+            
+        }
+       
+
+
+
+      
+    }
+}
